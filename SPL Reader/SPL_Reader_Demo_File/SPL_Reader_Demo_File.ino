8ef0560e7469f1047784cca6a3b4c49f784cd780
blierb <blierb.online@gmail.com>
2014-04-24 22:16:37 -0400

    Seem to be off by a scalar, but closer to functional.

--- SPL Reader/SPL_Reader_Demo_File/SPL_Reader_Demo_File.ino
+++ SPL Reader/SPL_Reader_Demo_File/SPL_Reader_Demo_File.ino
@@ -5,15 +5,24 @@
 
 
    /** Constants for Math **/
-const int DataSize = 32768;      // Samples per Gathering 
-const float Transfer = 6.3096;  // mV/pA (Microphone Transfer, See data sheet)
+/* Microphone / Preamp Characteristics */
+const int Sensitivity = -44;                     // dB re 1V/pA (See Data Sheet)
+const float Transfer = pow(10, Sensitivity/20); // mV/pA
+//const float Transfer = 6.3096;                // mV/pA (Microphone Transfer, See data sheet)
+
+const int Gain = 25;                // Preamp Gain
+const float dBGain = 20*log10(Gain);// In dB
+
+/* uC Charactaristics */
+const int DataSize = 32768/2;     // Samples per Gathering. Too big = overlfow stdDev
+const float InputVoltage = 3300;  // mV
+const float ADCrange = 1024;      // sample range
+const int average = ADCrange/2;   // Half of 1024. The signal is centered on "0" i.e. haflway
 
 /** Variables **/
-//int OldData[DataSize];
-long average = 512;
-double stdDevLong = 0;
-float dBSPL = 0;
-unsigned int i = 0;
+double stdDev = 0;                // Large floating point number
+float dBSPL = 0;                  // normal sized floating point number
+unsigned int i = 0;               // Coutning things!
 
 
  void setup ()
@@ -28,37 +37,30 @@ unsigned int i = 0;
  {
     // The code inside this loop will generate you one reading in about a half second
   
-    stdDevLong = 0;
+    stdDev = 0;
     dBSPL = 0;
     Serial.print("\n");
     Serial.println("Starting Read");
-    
+   
+    // Now Calculate Standard Deviation (RMS value, without the offset)
     for (i = 0; i < DataSize; i++)
     {
-      stdDevLong = stdDevLong + square(analogRead(0) - average); // Generate Sum
+      stdDev = stdDev + square(analogRead(0) - average); // Generate Sum part of standard deviation
     }	
-    //average = average / DataSize; // Division Part of Average
-
-
-    // Now Calculate Standard Deviation (RMS value, without the offset)
-//    for (i = 0; i < DataSize; i++)
-//    {
-//	stdDevLong = stdDevLong + square(OldData[i] - average); // Generate Sum
-//    }
-    stdDevLong = stdDevLong/(DataSize - 1);  // Standard Deviation
-    
+    stdDev = stdDev/(DataSize - 1);  // Division Part of Standard Deviation
     Serial.print("StdDeviation: \t");
-    Serial.println(stdDevLong);
+    Serial.println(stdDev);
     
     
-    // I hope I set up this order of operations so that it doesn't overflow anything.
-    // The Arduino is only 16-bit.
-    stdDevLong = 3300/1024 * (stdDevLong/125); 	// Now we have the Standard Deviation in mV
+    // Convert to mV post preamp from Arbitrary Units
+    stdDev = 3300/1024 * (stdDev); 	// Now we have the Standard Deviation in mV
     Serial.print("mV RMS: \t");
-    Serial.println(stdDevLong);
+    Serial.println(stdDev);
     
+    stdDev = stdDev / Gain;            // Devide by Preamp gain.
     // Do the Conversion:
-    dBSPL = (stdDevLong * 94) / Transfer; // This is the output!	
+    dBSPL = 20*log10(stdDev/Transfer) + Sensitivity + 94;
+    //dBSPL = (stdDev * 94) / Transfer; // mv * (dBSPL/Pa) / (mV / Pa) = dBSPL
     Serial.print("dBSPL: \t\t");
     Serial.println(dBSPL);
     //delay(1000);
