f70406f0bdf5fcb2ff4b3bb14c551a21b4d32942
blierb <blierb.online@gmail.com>
2014-05-08 00:01:54 -0400

    We get the correct RMS voltage (compared to a scope), however, the conversion to dBSPL isn't working. I'm still not sure why.

--- SPL Reader/SPL_Reader_Demo_File/SPL_Reader_Demo_File.ino
+++ SPL Reader/SPL_Reader_Demo_File/SPL_Reader_Demo_File.ino
@@ -6,17 +6,20 @@
 
    /** Constants for Math **/
 /* Microphone / Preamp Characteristics */
-const int Sensitivity = -44;                     // dB re 1V/pA (See Data Sheet)
-const float Transfer = pow(10, Sensitivity/20); // mV/pA
-//const float Transfer = 6.3096;                // mV/pA (Microphone Transfer, See data sheet)
+const int Sensitivity = -44;                         // dB re 1V/pA (See Data Sheet)
+const float Transfer = 1000*pow(10, Sensitivity/20); // mV/pA
+//const float Transfer = 6.3096;                     // mV/pA (Microphone Transfer, See data sheet)
 
-const int Gain = 25;                // Preamp Gain
-const float dBGain = 20*log10(Gain);// In dB
+const int Gain = 250;                          // Preamp Gain
+const float dBGain = 32;//20*log10(Gain);          // In dB
 
 /* uC Charactaristics */
-const int DataSize = 32768/2;     // Samples per Gathering. Too big = overlfow stdDev
-const float InputVoltage = 3300;  // mV
-const float ADCrange = 1024;      // sample range
+const int DataSize = 32768/8;                 // Samples per Gathering. Too big = overlfow stdDev
+const int DataMinus = DataSize - 1;
+const int InputVoltage = 3300;                // mV
+const int ADCrange = 1024;                    // sample range
+const float mVscalar = InputVoltage/ADCrange; // mV / (adc sample) ** Division is messy do this up here!
+
 const int average = ADCrange/2;   // Half of 1024. The signal is centered on "0" i.e. haflway
 
 /** Variables **/
@@ -45,25 +48,19 @@ unsigned int i = 0;               // Coutning things!
     // Now Calculate Standard Deviation (RMS value, without the offset)
     for (i = 0; i < DataSize; i++)
     {
-      stdDev = stdDev + square(analogRead(0) - average); // Generate Sum part of standard deviation
+      stdDev = stdDev + square(mVscalar * (analogRead(0) - average)); // Generate Sum part of standard deviation
     }	
-    stdDev = stdDev/(DataSize - 1);  // Division Part of Standard Deviation
-    Serial.print("StdDeviation: \t");
-    Serial.println(stdDev);
+    stdDev = sqrt(stdDev/DataMinus);  // Division Part of Standard Deviation
     
-    
-    // Convert to mV post preamp from Arbitrary Units
-    stdDev = 3300/1024 * (stdDev); 	// Now we have the Standard Deviation in mV
+    // This value should be correct
     Serial.print("mV RMS: \t");
     Serial.println(stdDev);
     
-    stdDev = stdDev / Gain;            // Devide by Preamp gain.
-    // Do the Conversion:
-    dBSPL = 20*log10(stdDev/Transfer) + Sensitivity + 94;
-    //dBSPL = (stdDev * 94) / Transfer; // mv * (dBSPL/Pa) / (mV / Pa) = dBSPL
+    // Do the Conversion (This part doesn't seem to work):
+    dBSPL = 20*log10(stdDev/Transfer) - dBGain + 94;
     Serial.print("dBSPL: \t\t");
     Serial.println(dBSPL);
-    //delay(1000);
+    delay(250);
    
    
  }
